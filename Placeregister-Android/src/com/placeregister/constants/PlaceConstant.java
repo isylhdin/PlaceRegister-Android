67610c7aba7f68593ee96d3aa3c6bf88b8706d54
Yoann <yoann_guerin@hotmail.com>
2014-03-02 16:58:44 +0100

    URL added to use backend deployed on heroku

--- Placeregister-Android/src/com/placeregister/constants/PlaceConstant.java
+++ Placeregister-Android/src/com/placeregister/constants/PlaceConstant.java
@@ -3,28 +3,36 @@ package com.placeregister.constants;
 public class PlaceConstant {
 
 	/**
-	 * Raspberry local jetty server url. Another context is added by default (ie war's name)
+	 * Raspberry local jetty server url. Another context is added by default (ie
+	 * war's name)
 	 */
 	private static final String BACK_END_RASPBERRY_LOCAL_URL = "http://192.168.0.11:8080/Placeregister-backend-0.0.1-SNAPSHOT/rest/place/service/";
-	
+
 	/**
 	 * Embedded jetty server launched with maven/eclipse
 	 */
 	private static final String BACK_END_URL = "http://192.168.0.7:8080/rest/place/service/";
-	
+
+	/**
+	 * Heroku url
+	 */
+	private static final String BACK_END_HEROKU_URL = "http://sheltered-wildwood-2400.herokuapp.com/rest/place/service/";
+
 	/**
 	 * Google places web service url
 	 */
 	public static final String PLACES_SEARCH_URL = "maps.googleapis.com";
-	
+
 	/**
 	 * Back end url to retrieve already visited places
 	 */
-	public static final String GET_VISITED_PLACES_URL =  BACK_END_URL + "get/visited/places";
-	
+	public static final String GET_VISITED_PLACES_URL = BACK_END_HEROKU_URL
+			+ "get/visited/places";
+
 	/**
 	 * Back end url to add a new place in DataBase
 	 */
-	public static final String ADD_PLACE_URL = BACK_END_URL + "add/place";
-	
+	public static final String ADD_PLACE_URL = BACK_END_HEROKU_URL
+			+ "add/place";
+
 }
